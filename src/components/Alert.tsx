be9dbc7b9fd6e0b5f69e4659334df3731a16a242
minagishl <mvxd568.co@icloud.com>
2025-06-10 01:02:37 +0900

    fix: add missing classes

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -27,25 +27,25 @@ export default function Alert({
 }: AlertProps) {
   const variantClasses: Record<AlertVariant, string> = {
     dark: soft
-      ? "bg-gray-100 border-gray-200 text-gray-800 dark:bg-white/10 dark:border-white/20 dark:text-white"
+      ? "bg-gray-100 border-gray-200 text-gray-800 dark:bg-white/10 dark:border-white/20 dark:text-white border"
       : "bg-gray-800 text-white dark:bg-white dark:text-neutral-800",
     secondary: soft
-      ? "bg-gray-50 border-gray-200 text-gray-600 dark:bg-white/10 dark:border-white/10 dark:text-neutral-400"
+      ? "bg-gray-50 border-gray-200 text-gray-600 dark:bg-white/10 dark:border-white/10 dark:text-neutral-400 border"
       : "bg-gray-500 text-white",
     info: soft
-      ? "bg-blue-100 border-blue-200 text-blue-800 dark:bg-blue-800/10 dark:border-blue-900 dark:text-blue-500"
+      ? "bg-blue-100 border-blue-200 text-blue-800 dark:bg-blue-800/10 dark:border-blue-900 dark:text-blue-500 border"
       : "bg-blue-600 text-white dark:bg-blue-500",
     success: soft
-      ? "bg-teal-100 border-teal-200 text-teal-800 dark:bg-teal-800/10 dark:border-teal-900 dark:text-teal-500"
+      ? "bg-teal-100 border-teal-200 text-teal-800 dark:bg-teal-800/10 dark:border-teal-900 dark:text-teal-500 border"
       : "bg-teal-500 text-white",
     danger: soft
-      ? "bg-red-100 border-red-200 text-red-800 dark:bg-red-800/10 dark:border-red-900 dark:text-red-500"
+      ? "bg-red-100 border-red-200 text-red-800 dark:bg-red-800/10 dark:border-red-900 dark:text-red-500 border"
       : "bg-red-500 text-white",
     warning: soft
-      ? "bg-yellow-100 border-yellow-200 text-yellow-800 dark:bg-yellow-800/10 dark:border-yellow-900 dark:text-yellow-500"
+      ? "bg-yellow-100 border-yellow-200 text-yellow-800 dark:bg-yellow-800/10 dark:border-yellow-900 dark:text-yellow-500 border"
       : "bg-yellow-500 text-white",
     light: soft
-      ? "bg-white/10 border-white/10 text-white"
+      ? "bg-white/10 border-white/10 text-white border"
       : "bg-white text-gray-600",
   };
 
@@ -65,9 +65,9 @@ export default function Alert({
     <div
       className={cn(
         "mt-2 text-sm rounded-lg p-4",
-        variantClasses[variant],
         borderClasses,
-        borderColorClasses[variant],
+        variantClasses[variant],
+        !soft && borderColorClasses[variant],
         className
       )}
       role="alert"
