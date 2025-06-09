412ac7adb7222839369a84c5dc83c0d57085331c
minagishl <mvxd568.co@icloud.com>
2025-06-09 21:46:05 +0900

    chore: remove unused imports

--- src/stories/AvatarGroup.stories.tsx
+++ src/stories/AvatarGroup.stories.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import type { Meta, StoryObj } from "@storybook/react-vite";
 import { AvatarGroup } from "../components/AvatarGroup";
 import { Avatar } from "../components/Avatar";
